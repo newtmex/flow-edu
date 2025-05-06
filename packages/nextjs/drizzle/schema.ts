5d633adc9a6f7af971db4d7a65f4fa4ee87e2628
newtmex <brightomex@gmail.com>
2025-05-06 13:57:51 +0100

    feat: add txs_on_arb and job_locks tables with enums for origin and status

--- packages/nextjs/drizzle/schema.ts
+++ packages/nextjs/drizzle/schema.ts
@@ -1,4 +1,4 @@
-import { bigint, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { bigint, json, pgEnum, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const walletBindings = pgTable("wallet_bindings", {
   userAddress: text().primaryKey(),
@@ -8,4 +8,38 @@ export const walletBindings = pgTable("wallet_bindings", {
   message: text().unique().notNull(),
   timestamp: bigint({ mode: "number" }).notNull(),
   createdAt: timestamp().defaultNow(),
+});
+
+export enum Origin {
+  BSC = "BSC",
+  EDUChain = "EDUChain",
+}
+
+export enum ArbTxStatus {
+  Pending = "pending",
+  Handled = "handled",
+  Ignored = "ignored",
+}
+
+export function enumToPgEnum<T extends Record<string, any>>(myEnum: T): [T[keyof T], ...T[keyof T][]] {
+  return Object.values(myEnum).map((value: any) => `${value}`) as any;
+}
+
+export const originEnum = pgEnum("origin", enumToPgEnum(Origin));
+export const arbTxStatusEnum = pgEnum("status", enumToPgEnum(ArbTxStatus));
+
+export const txsOnArb = pgTable("txs_on_arb", {
+  originHash: varchar({ length: 66 }).primaryKey(),
+  arbHash: json().$type<string[]>().default([]),
+  to: varchar({ length: 42 }).notNull(),
+  value: bigint({ mode: "number" }).notNull(),
+  origin: originEnum().notNull(), // "BSC" | "EDUChain"
+  status: arbTxStatusEnum().default(ArbTxStatus.Pending), // pending | handled | ignored | failed
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const jobLocks = pgTable("job_locks", {
+  job: text("job").primaryKey(), // e.g., "processPendingTxs"
+  lockedAt: timestamp("locked_at").notNull(),
 });
