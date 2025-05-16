76e9a13e6f1a177bc1e4b0217c660319e13226b5
newtmex <brightomex@gmail.com>
2025-05-16 05:57:15 +0100

    refactor: enhance removeAddressZeroPadding function to validate and normalize address format

--- packages/nextjs/app/api/edu-transfer-webhook/route.ts
+++ packages/nextjs/app/api/edu-transfer-webhook/route.ts
@@ -2,10 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 import { normalizeAddresses } from "../lib/drizzleUtils";
 import handleArbTxs from "./arbTxs";
 import { eq, or } from "drizzle-orm";
+import { Hex, bytesToHex, getAddress, hexToBytes } from "viem";
 import { db } from "~~/drizzle/db";
 import { Origin, txsOnBsc, txsOnEduChain, walletBindings } from "~~/drizzle/schema";
 
-const removeAddressZeroPadding = (address: string) => "0x" + BigInt(address).toString(16);
+const removeAddressZeroPadding = (address: Hex) => {
+  const bytes = hexToBytes(address);
+  if (bytes.length < 32) throw new Error("Invalid padded address length");
+
+  const sliced = bytes.slice(0, 20); // Take first 20 bytes
+  return getAddress(bytesToHex(sliced)); // Returns checksummed address
+};
 
 export const POST = async (req: NextRequest) => {
   let body: any;
